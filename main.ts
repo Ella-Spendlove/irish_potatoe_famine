7588f81f16cc86003a1d26e4b8fa76da50e692d4
Ella-Spendlove <102975888+Ella-Spendlove@users.noreply.github.com>
2022-04-04 18:37:32 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ input.onButtonPressed(Button.A, function () {
     led.stopAnimation()
     basic.showString("No more potatoes :(")
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showString("Potato again")
+    music.playMelody("G B A G C5 B A B ", 120)
+})
 basic.showLeds(`
     . # # # .
     . # # # .
